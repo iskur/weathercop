88ea4371a0a2311fe2346887f172b21d78286e7e
iskur <32363199+iskur@users.noreply.github.com>
2016-03-27 14:37:07 +0200

    ENH: autocopulas of order k + plotting in __main__

--- find_copula.py
+++ find_copula.py
@@ -36,16 +36,23 @@ def mml_serial(ranks_u, ranks_v, cops):
     return max(fitted_cops)
 
 
-def mml_kdim(data, cops):
+def mml_kdim(data, cops, k=1):
     K = len(data)
     fitted_cops = {}
     for i in range(K):
-        ranks_u = data[i, :-1]
+        if k > 0:
+            ranks_u = data[i, :-k]
+        else:
+            ranks_u = data[i]
         for j in range(K):
             if i == j:
                 continue
-            ranks_v = data[j, 1:]
+            if k > 0:
+                ranks_v = data[j, k:]
+            else:
+                ranks_v = data[j]
             fitted_cops[i, j] = mml_serial(ranks_u, ranks_v, cops)
+            print()
     return fitted_cops
 
 
@@ -57,22 +64,61 @@ if __name__ == '__main__':
                                  "vg_data.npz")
     with np.load(data_filepath) as saved:
         data_summer = saved["summer"]
+        data_winter = saved["winter"]
+    data = np.hstack((data_summer, data_winter))
+    # data = data_summer
     # ranks_u_tm1 = copulae.rel_ranks(data_summer[5, :-1])
     # ranks_rh = copulae.rel_ranks(data_summer[4, 1:])
     # # fitted_cops = mml(ranks_u_tm1, ranks_rh, copulae.all_cops)
     # best_cop = mml_serial(ranks_u_tm1, ranks_rh, copulae.all_cops)
     # print(best_cop)
-    data_ranks = np.array([copulae.rel_ranks(row) for row in data_summer])
-    fitted_cops = mml_kdim(data_ranks, copulae.all_cops)
 
-    K = len(data_ranks)
-    fig, axs = plt.subplots(K, K, subplot_kw=dict(aspect="equal"))
-    for i in range(K):
-        for j in range(K):
-            if i == j:
-                axs[i, j].set_axis_off()
-            else:
-                cop = fitted_cops[(i, j)]
-                cop.plot_density(ax=axs[i, j])
-                axs[i, j].set_title(cop.name)
+    def plot_matrix(data):
+        data_ranks = np.array([stats.rel_ranks(row) for row in data])
+        fitted_cops = mml_kdim(data_ranks, copulae.all_cops, k=1)
+
+        K = len(data_ranks)
+        fig, axs = plt.subplots(K, K, subplot_kw=dict(aspect="equal"),
+                                figsize=(15, 15))
+        for i in range(K):
+            for j in range(K):
+                ax = axs[i, j]
+                if i == j:
+                    ax.set_axis_off()
+                    ax.text(.5, .5, cop_conf.var_names[i],
+                            horizontalalignment="center",
+                            verticalalignment="center")
+                else:
+                    cop = fitted_cops[(i, j)]
+                    cop.plot_density(ax=ax, kind="contour", scatter=False)
+                    ranks_x, ranks_y = data_ranks[i], data_ranks[j]
+                    ax.scatter(ranks_x, ranks_y,
+                               marker="o",
+                               facecolor=(0, 0, 0, 0),
+                               edgecolor=(0, 0, 0, .1))
+                    ax.set_title("%s (%.2f)" % (cop.name[len("fitted "):],
+                                                cop.likelihood))
+                    rho = stats.spearmans_rank(ranks_x, ranks_y)
+                    asy1 = stats.asymmetry1(ranks_x, ranks_y)
+                    asy2 = stats.asymmetry2(ranks_x, ranks_y)
+                    t_kwds = dict(fontsize=18,
+                                  horizontalalignment="center",
+                                  verticalalignment="center")
+                    ax.text(.5, .6, r"$\rho = %.2f$" % rho, **t_kwds)
+                    ax.text(.5, .5, r"$a_1 = %.2f$" % (100 * asy1),  **t_kwds)
+                    ax.text(.5, .4, r"$a_2 = %.2f$" % (100 * asy2), **t_kwds)
+                    ax.set_xticklabels([])
+                    ax.set_yticklabels([])
+                    ax.set_xticks([])
+                    ax.set_yticks([])
+                    # ax.text(.5, .5, "like: %.2f" % cop.likelihood,
+                    #                horizontalalignment="center",
+                    #                verticalalignment="center",
+                    #                color="red")
+        return fig, axs
+
+    fig, axs = plot_matrix(data_summer)
+    fig.suptitle("summer")
+    fig, axs = plot_matrix(data_winter)
+    fig.suptitle("winter")
     plt.show()
