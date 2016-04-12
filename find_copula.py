0c088968e2f286c989980ebdb2a9e3c722166340
iskur <32363199+iskur@users.noreply.github.com>
2016-04-12 08:56:05 +0200

    REF: copula plot matrix from __main__ to def

--- find_copula.py
+++ find_copula.py
@@ -68,10 +68,54 @@ def mml_kdim(data, cops=copulae.all_cops, k=1):
     return fitted_cops
 
 
+def plot_matrix(data, kind="contourf"):
+    data_ranks = np.array([stats.rel_ranks(row) for row in data])
+    fitted_cops = mml_kdim(data_ranks, copulae.all_cops, k=1)
+
+    K = len(data_ranks)
+    fig, axs = plt.subplots(K, K, subplot_kw=dict(aspect="equal"),
+                            figsize=(15, 15))
+    for i in range(K):
+        for j in range(K):
+            ax = axs[i, j]
+            if i == j:
+                ax.set_axis_off()
+                ax.text(.5, .5, cop_conf.var_names[i],
+                        horizontalalignment="center",
+                        verticalalignment="center")
+            else:
+                cop = fitted_cops[(i, j)]
+                cop.plot_density(ax=ax, kind=kind, scatter=False)
+                ranks_x, ranks_y = data_ranks[i], data_ranks[j]
+                ax.scatter(ranks_x, ranks_y,
+                           marker="o",
+                           facecolor=(0, 0, 0, 0),
+                           edgecolor=(0, 0, 0, .1))
+                ax.set_title("%s (%.2f)" % (cop.name[len("fitted "):],
+                                            cop.likelihood))
+                rho = stats.spearmans_rank(ranks_x, ranks_y)
+                asy1 = stats.asymmetry1(ranks_x, ranks_y)
+                asy2 = stats.asymmetry2(ranks_x, ranks_y)
+                t_kwds = dict(fontsize=25,
+                              horizontalalignment="center",
+                              verticalalignment="center",
+                              color="red")
+                ax.text(.5, .6, r"$\rho = %.2f$" % rho, **t_kwds)
+                ax.text(.5, .5, r"$a_1 = %.2f$" % (100 * asy1),  **t_kwds)
+                ax.text(.5, .4, r"$a_2 = %.2f$" % (100 * asy2), **t_kwds)
+                ax.set_xticklabels([])
+                ax.set_yticklabels([])
+                ax.set_xticks([])
+                ax.set_yticks([])
+                # ax.text(.5, .5, "like: %.2f" % cop.likelihood,
+                #                horizontalalignment="center",
+                #                verticalalignment="center",
+                #                color="red")
+    return fig, axs
+
+
 if __name__ == '__main__':
     import os
-    import numpy as np
-    import matplotlib.pyplot as plt
     data_filepath = os.path.join(cop_conf.weathercop_dir, "code",
                                  "vg_data.npz")
     with np.load(data_filepath) as saved:
@@ -84,50 +128,6 @@ if __name__ == '__main__':
     # # fitted_cops = mml(ranks_u_tm1, ranks_rh, copulae.all_cops)
     # best_cop = mml_serial(ranks_u_tm1, ranks_rh, copulae.all_cops)
     # print(best_cop)
-
-    def plot_matrix(data):
-        data_ranks = np.array([stats.rel_ranks(row) for row in data])
-        fitted_cops = mml_kdim(data_ranks, copulae.all_cops, k=1)
-
-        K = len(data_ranks)
-        fig, axs = plt.subplots(K, K, subplot_kw=dict(aspect="equal"),
-                                figsize=(15, 15))
-        for i in range(K):
-            for j in range(K):
-                ax = axs[i, j]
-                if i == j:
-                    ax.set_axis_off()
-                    ax.text(.5, .5, cop_conf.var_names[i],
-                            horizontalalignment="center",
-                            verticalalignment="center")
-                else:
-                    cop = fitted_cops[(i, j)]
-                    cop.plot_density(ax=ax, kind="contour", scatter=False)
-                    ranks_x, ranks_y = data_ranks[i], data_ranks[j]
-                    ax.scatter(ranks_x, ranks_y,
-                               marker="o",
-                               facecolor=(0, 0, 0, 0),
-                               edgecolor=(0, 0, 0, .1))
-                    ax.set_title("%s (%.2f)" % (cop.name[len("fitted "):],
-                                                cop.likelihood))
-                    rho = stats.spearmans_rank(ranks_x, ranks_y)
-                    asy1 = stats.asymmetry1(ranks_x, ranks_y)
-                    asy2 = stats.asymmetry2(ranks_x, ranks_y)
-                    t_kwds = dict(fontsize=18,
-                                  horizontalalignment="center",
-                                  verticalalignment="center")
-                    ax.text(.5, .6, r"$\rho = %.2f$" % rho, **t_kwds)
-                    ax.text(.5, .5, r"$a_1 = %.2f$" % (100 * asy1),  **t_kwds)
-                    ax.text(.5, .4, r"$a_2 = %.2f$" % (100 * asy2), **t_kwds)
-                    ax.set_xticklabels([])
-                    ax.set_yticklabels([])
-                    ax.set_xticks([])
-                    ax.set_yticks([])
-                    # ax.text(.5, .5, "like: %.2f" % cop.likelihood,
-                    #                horizontalalignment="center",
-                    #                verticalalignment="center",
-                    #                color="red")
-        return fig, axs
 
     fig, axs = plot_matrix(data_summer)
     fig.suptitle("summer")
