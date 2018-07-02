f1eb86b7a3b6b150356de2be8247288af7b435dd
iskur <32363199+iskur@users.noreply.github.com>
2018-07-02 10:26:13 +0200

    TST

--- tests/test_vine.py
+++ tests/test_vine.py
@@ -1,12 +1,17 @@
+import time
+from collections import OrderedDict
+
 import numpy as np
-import numpy.testing as npt
+import dill
 import matplotlib.pyplot as plt
+import numpy.testing as npt
+import pandas as pd
 from scipy import stats as spstats
 
-from weathercop.vine import RVine, CVine, stats, vg_ph
-from weathercop import plotting
-from lhglib.contrib.time_series_analysis import distributions as dists
 from lhglib.contrib import dirks_globals as my
+from lhglib.contrib.time_series_analysis import distributions as dists
+from weathercop import plotting
+from weathercop.vine import CVine, RVine, vg_ph
 
 
 class Test(npt.TestCase):
@@ -50,7 +55,8 @@ class Test(npt.TestCase):
         weights = "tau"
 
         self.rvine = RVine(self.data_ranks, varnames=self.varnames,
-                           weights=weights, verbose=True)
+                           weights=weights, verbose=self.verbose,
+                           debug=self.verbose)
         self.rsim = self.rvine.simulate(T=3 * T)
         self.rsim_normal = np.array([dists.norm.ppf(values,
                                                     mu=source.mean(),
@@ -58,9 +64,10 @@ class Test(npt.TestCase):
                                      for values, source
                                      in zip(self.rsim, self.data_normal)])
         self.rquantiles = self.rvine.quantiles()
- 
+
         self.cvine = CVine(self.data_ranks, varnames=self.varnames,
-                           weights=weights, verbose=True)
+                           weights=weights, verbose=self.verbose,
+                           debug=self.verbose, tau_min=0)
         self.csim = self.cvine.simulate(T=3 * T)
         self.csim_normal = np.array([dists.norm.ppf(values,
                                                     mu=source.mean(),
@@ -69,28 +76,33 @@ class Test(npt.TestCase):
                                     in zip(self.csim, self.data_normal)])
         self.cquantiles = self.cvine.quantiles()
 
-    def test_likelihood_tree(self):
-        if self.verbose:
-            print("Testing tree construction with likelihood as weight")
-        rvine = RVine(self.data_ranks, varnames=self.varnames,
-                      verbose=self.verbose, weights="likelihood")
-        fig, ax = rvine.plot(edge_labels="copulas")
-        fig.suptitle("likelihood")
-        fig, ax = rvine.plot_tplom()
-        fig.suptitle("likelihood")
-        fig, ax = rvine.plot_qqplom()
-        fig.suptitle("likelihood")
+    def test_serialize(self):
+        for vine in (self.cvine, self.rvine):
+            dill_str = dill.dumps(vine)
+            vine_recovered = dill.loads(dill_str)
 
-        rvine = RVine(self.data_ranks, varnames=self.varnames,
-                      verbose=self.verbose, weights="tau")
-        fig, ax = self.rvine.plot(edge_labels="copulas")
-        fig.suptitle("tau")
-        fig, ax = self.rvine.plot_tplom()
-        fig.suptitle("tau")
-        fig, ax = self.rvine.plot_qqplom()
-        fig.suptitle("tau")
-        plt.show()
-        
+    # def test_likelihood_tree(self):
+    #     if self.verbose:
+    #         print("Testing tree construction with likelihood as weight")
+    #     rvine = RVine(self.data_ranks, varnames=self.varnames,
+    #                   verbose=self.verbose, weights="likelihood")
+    #     fig, ax = rvine.plot(edge_labels="copulas")
+    #     fig.suptitle("likelihood")
+    #     fig, ax = rvine.plot_tplom()
+    #     fig.suptitle("likelihood")
+    #     fig, ax = rvine.plot_qqplom()
+    #     fig.suptitle("likelihood")
+
+    #     rvine = RVine(self.data_ranks, varnames=self.varnames,
+    #                   verbose=self.verbose, weights="tau")
+    #     fig, ax = self.rvine.plot(edge_labels="copulas")
+    #     fig.suptitle("tau")
+    #     fig, ax = self.rvine.plot_tplom()
+    #     fig.suptitle("tau")
+    #     fig, ax = self.rvine.plot_qqplom()
+    #     fig.suptitle("tau")
+    #     plt.show()
+
     def test_rsimulate(self):
         if self.verbose:
             print("Covariance matrix of RVine simulation")
@@ -113,10 +125,12 @@ class Test(npt.TestCase):
                                     np.cov(self.data_normal),
                                     decimal=1)
         except AssertionError:
-            plotting.ccplom(self.rsim, k=0, kind="img",
-                            title="simulated from CVine")
-            plotting.ccplom(self.data_ranks, k=0, kind="img", title="observed")
-            plt.show()
+            print("Obs \n", np.corrcoef(self.data_ranks).round(3))
+            print("Sim \n", np.corrcoef(self.cquantiles).round(3))
+            # plotting.ccplom(self.rsim, k=0, kind="img",
+            #                 title="simulated from CVine")
+            # plotting.ccplom(self.data_ranks, k=0, kind="img", title="observed")
+            # plt.show()
             raise
 
     def test_rquantiles(self):
@@ -197,18 +211,22 @@ class Test(npt.TestCase):
             print("Quantile simulation roundtrip of CVine")
         corr_exp = np.zeros_like(self.cov)
         corr_exp.ravel()[::self.K+1] = 1
-        try:
-            npt.assert_almost_equal(np.corrcoef(self.cquantiles),
-                                    corr_exp,
-                                    decimal=2)
-        except AssertionError:
-            plotting.ccplom(self.cquantiles, k=0, kind="img",
-                            title="cquantiles")
-            plotting.ccplom(self.data_ranks, k=0, kind="img",
-                            title="copula_input")
-            self.cvine.plot_qqplom()
-            # plt.show()
-            # raise
+        sim = self.cvine.simulate(randomness=self.cquantiles)
+        # corr_act = np.corrcoef(self.cquantiles)
+        # try:
+        #     npt.assert_almost_equal(corr_act, corr_exp, decimal=2)
+        # except AssertionError:
+        #     print("Obs \n", np.corrcoef(self.data_ranks).round(3))
+        #     print("Sim \n", np.corrcoef(sim).round(3))
+        #     # cc_kwds = dict(k=0, kind="img", varnames=self.varnames)
+        #     # plotting.ccplom(self.cquantiles, title="cquantiles",
+        #     #                 **cc_kwds)
+        #     # plotting.ccplom(self.data_ranks, title="copula_input",
+        #     #                 **cc_kwds)
+        #     # plotting.ccplom(sim, title="copula output", **cc_kwds)
+        #     # self.cvine.plot_qqplom()
+        #     # plt.show()
+        #     raise
 
         # # does it matter if we specify the ranks explicitly?
         # quantiles_self = self.cvine.quantiles(ranks=self.cvine.ranks)
@@ -222,14 +240,24 @@ class Test(npt.TestCase):
         #     plt.show()
         #     raise
 
-        sim = self.cvine.simulate(randomness=self.cquantiles)
+        print(self.cvine)
+        print(self.cvine.varnames)
+        print(self.cvine.varnames_old)
         try:
             npt.assert_almost_equal(sim, self.data_ranks, decimal=2)
         except AssertionError:
+            print("Obs\n",
+                  pd.DataFrame(np.corrcoef(self.data_ranks).round(3),
+                               index=self.varnames,
+                               columns=self.varnames))
+            print("Sim\n", pd.DataFrame(np.corrcoef(sim).round(3),
+                                        index=self.varnames,
+                                        columns=self.varnames))
             fig, axs = plt.subplots(nrows=self.K, sharex=True)
             for i, ax in enumerate(axs):
                 ax.plot(self.data_ranks[i])
                 ax.plot(sim[i], "--")
+                ax.set_title(self.varnames[i])
 
             fig, axs = plt.subplots(nrows=int(np.sqrt(self.K) + 1),
                                     ncols=int(np.sqrt(self.K)),
@@ -254,25 +282,59 @@ class Test(npt.TestCase):
             print("Zero correlation matrix of RVine quantiles")
         corr_exp = np.zeros_like(self.cov)
         corr_exp.ravel()[::self.K+1] = 1
+        corr_act = np.corrcoef(self.cquantiles)
         try:
-            npt.assert_almost_equal(np.corrcoef(self.cquantiles),
-                                    corr_exp,
-                                    decimal=1)
+            npt.assert_almost_equal(corr_act, corr_exp, decimal=1)
         except AssertionError:
-            plotting.ccplom(self.cquantiles, k=0, kind="img",
-                            title="cquantiles")
-            plotting.ccplom(self.data_ranks, k=0, kind="img",
-                            title="copula_input")
-            self.cvine.plot_qqplom()
-            fig, axs = plt.subplots(nrows=self.K, ncols=1, sharex=True)
-            plt.show()
+            print("Quantiles corr \n", corr_act.round(3))
+            # plotting.ccplom(self.cquantiles, k=0, kind="img",
+            #                 title="cquantiles")
+            # plotting.ccplom(self.data_ranks, k=0, kind="img",
+            #                 title="copula_input")
+            # self.cvine.plot_qqplom()
+            # fig, axs = plt.subplots(nrows=self.K, ncols=1, sharex=True)
+            # plt.show()
             raise
 
+    def test_seasonal_yearly(self):
+        if self.verbose:
+            print("Seasonal Vine with singe year VG data")
+            import vg
+            from vg import vg_plotting, vg_base
+            import config_konstanz_disag as conf
+        vg.conf = vg_plotting.conf = vg_base.conf = conf
+        # met_vg = vg.VG(("theta", "ILWR", "rh", "R"), verbose=True)
+        met_vg = vg.VG(("theta", "Qsw", "ILWR", "rh", "u", "v"), verbose=True)
+        ranks = np.array([spstats.norm.cdf(values)
+                          for values in met_vg.data_trans])
+        T = ranks.shape[1]
+        for ranks_ in (ranks[:, :T//2], ranks[:, T//2:]):
+            cvine = RVine(ranks_,
+                          varnames=met_vg.var_names,
+                          # dtimes=met_vg.times[year_mask],
+                          # weights="likelihood"
+                          )
+        # year_first = met_vg.times[0].year
+        # year_last = met_vg.times[-1].year
+        # years = np.array([dtime.year for dtime in met_vg.times])
+        # ranks = np.array([spstats.norm.cdf(values)
+        #                   for values in met_vg.data_trans])
+        # for year in range(year_first, year_last + 1):
+        #     year_mask = years == year
+        #     cvine = RVine(ranks[:, year_mask],
+        #                   varnames=met_vg.var_names,
+        #                   # dtimes=met_vg.times[year_mask],
+        #                   # weights="likelihood"
+        #                   )
+            fig, axs = cvine.plot(edge_labels="copulas")
+        plt.show()
+        
     def test_seasonal(self):
         if self.verbose:
             print("Seasonal Vine with VG data")
-        from lhglib.contrib.veathergenerator import vg, vg_plotting, vg_base
-        from lhglib.contrib.veathergenerator import config_konstanz_disag as conf
+            import vg
+            from vg import vg_plotting, vg_base
+            import config_konstanz_disag as conf
         vg.conf = vg_plotting.conf = vg_base.conf = conf
         # met_vg = vg.VG(("theta", "ILWR", "rh", "R"), verbose=True)
         met_vg = vg.VG(("theta", "ILWR"), verbose=True)
@@ -302,24 +364,71 @@ class Test(npt.TestCase):
     def test_seasonal_ph(self):
         if self.verbose:
             print("VG with phase randomization")
-        from lhglib.contrib.veathergenerator import vg
-        from lhglib.contrib.veathergenerator import config_konstanz_disag as conf
-        from lhglib.contrib.veathergenerator import vg_plotting
-        vg.conf = vg.vg_base.conf = vg_plotting.conf = conf
+        import vg
+        import config_konstanz as conf
+        vg.conf = vg.vg_base.conf = vg.vg_plotting.conf = conf
         met_vg = vg.VG((
                         # 'R',
                         'theta', 'ILWR', 'rh',
-                        # 'u', 'v'
+                        'u', 'v'
                        ),
                        # refit=True,
                        verbose=True)
         met_vg.fit(p=3)
-        vg_ph.clear_cache()
-        simt, sim = met_vg.simulate(
-            # theta_incr=4, mean_arrival=7,
-            # disturbance_std=5,
-            sim_func=vg_ph)
-        # met_vg.plot_all()
+        theta_incr = .1
+        np.random.seed(0)
+        n_realizations = 4
+        means_norm0, means0, means1 = [], [], []
+        before = time.time()
+        for _ in range(n_realizations):
+            simt0, sim0 = met_vg.simulate( #  theta_incr=0.,
+                                          sim_func=vg_ph)
+            means_norm0 += [met_vg.sim.mean(axis=1)]
+            simt1, sim1 = met_vg.simulate(theta_incr=theta_incr,
+                                          sim_func=vg_ph)
+            prim_i = met_vg.primary_var_ii
+            means0 += [sim0.mean(axis=1)]
+            # means1 += [sim1[prim_i].mean()]
+            # means0 += [sim0[prin_i].mean()]
+            means1 += [sim1.mean(axis=1)]
+        print(time.time() - before)
+        means0 = np.mean(means0, axis=0)
+        means1 = np.mean(means1, axis=0)
+        # print(means0[prim_i] + theta_incr, means1[prim_i])
+        # print(means0, means1)
+        np.set_printoptions(suppress=True)
+        import pandas as pd
+        data_dict = OrderedDict(
+            (('data_norm', met_vg.data_trans.mean(axis=1)),
+             ('sim_norm', np.mean(means_norm0, axis=0)),
+             ('data', (met_vg.data_raw / met_vg.sum_interval).mean(axis=1)),
+             ('sim', means0),
+             )
+            )
+        means_df = pd.DataFrame(data_dict,
+                                index=met_vg.var_names)
+        print(means_df)
+        npt.assert_almost_equal(met_vg.data_trans.mean(axis=1),
+                                np.mean(means_norm0, axis=0), decimal=2)
+        means0[prim_i] += theta_incr
+        means_obs = met_vg.data_raw.mean(axis=1) / 24
+        means_obs[prim_i] += theta_incr
+        npt.assert_almost_equal(means_obs[prim_i], means1[prim_i],
+                                decimal=2)
+        npt.assert_almost_equal(means0[prim_i], means1[prim_i],
+                                decimal=2)
+
+        # vg_ph.clear_cache()
+        # for _ in range(10):
+        #     simt, sim = met_vg.simulate(
+        #         theta_incr=4, mean_arrival=7,
+        #         disturbance_std=5,
+        #         sim_func=vg_ph)
+        # vg_ph.vine.plot(edge_labels="copulas")
+        # # vg_ph.vine.plot_tplom()
+        # vg_ph.vine.plot_qqplom()
+        # vg_ph.vine.plot_seasonal()
+        # # met_vg.plot_all()
         # plt.show()
 
 
