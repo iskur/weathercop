50dce4a3b6465b622701d6b60cefe38f517e464c
iskur <32363199+iskur@users.noreply.github.com>
2018-07-06 09:14:54 +0200

    CONF: shelve cache files esp. for vines and vg instances

--- weathercop/cop_conf.py
+++ weathercop/cop_conf.py
@@ -1,24 +1,25 @@
 """Central file to keep the same configuration in all weathercop code.
 """
-import os
 from pathlib import Path
 try:
     from lhglib.contrib.dirks_globals import ADict
 except ImportError:
     from weathercop.tools import ADict
 
+PROFILE = False
 home = Path.home()
 weathercop_dir = home / "WeatherCop"
 # ufunc_tmp_dir = "home/dirk/workspace/python/weathercop/ufunc_tmp"
 code_dir = home / "workspace/python/weathercop/code"
 cache_dir = code_dir / "cache"
 ufunc_tmp_dir = cache_dir / "ufunc_tmp_dir"
-# has some good vintages
-vineyard = cache_dir / "vineyard"
-sympy_cache = ufunc_tmp_dir / "sympy_cache.she"
-theano_cache = ufunc_tmp_dir / "theano_cache.she"
+sympy_cache = cache_dir / "sympy_cache.she"
+theano_cache = cache_dir / "theano_cache.she"
+vine_cache = cache_dir / "vine_cache.she"
+vgs_cache_dir = cache_dir / "vgs_cache"
 varnames = ("R", "theta", "Qsw", "ILWR", "rh", "u", "v")
 
+vgs_cache_dir.mkdir(exist_ok=True)
 
 class VGG:
     init = ADict(var_names=varnames, verbose=True,
@@ -34,3 +35,7 @@ class Vg(VGG):
     """VG configuration."""
     pass
 vg = Vg()
+
+
+# class CV:
+#     init = ADict()
