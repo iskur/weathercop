4e53b3a2aa9dc6beac8ed8b7d9cf3e652d352138
iskur <32363199+iskur@users.noreply.github.com>
2021-09-16 13:05:08 +0200

    CONF: minor

--- weathercop/cop_conf.py
+++ weathercop/cop_conf.py
@@ -3,36 +3,19 @@
 from pathlib import Path
 from weathercop.tools import ADict
 
+# setting profile to True disables parallel computation, allowing for
+# profiling and debugging
 PROFILE = False
 home = Path.home()
-weathercop_dir = home / "WeatherCop"
+weathercop_dir = home / "Projects" / "WeatherCop"
 # ufunc_tmp_dir = cache_dir / "ufunc_tmp_dir"
 script_home = Path(__file__).parent
 ufunc_tmp_dir = script_home / "ufuncs"
 sympy_cache = ufunc_tmp_dir / "sympy_cache.she"
 cache_dir = weathercop_dir / "cache"
-theano_cache = cache_dir / "theano_cache.she"
+theano_cache = ufunc_tmp_dir / "theano_cache.she"
 vine_cache = cache_dir / "vine_cache.she"
 vgs_cache_dir = cache_dir / "vgs_cache"
 varnames = ("R", "theta", "Qsw", "ILWR", "rh", "u", "v")
 
 vgs_cache_dir.mkdir(exist_ok=True, parents=True)
-
-class VGG:
-    init = ADict(var_names=varnames, verbose=True,
-                 non_rain=(
-                     #  "theta",
-                     "ILWR", "Qsw", "rh", "u", "v"))
-    fit = ADict(p=3)
-    sim = ADict()
-    dis = ADict()
-
-
-class Vg(VGG):
-    """VG configuration."""
-    pass
-vg = Vg()
-
-
-# class CV:
-#     init = ADict()
