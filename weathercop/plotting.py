6f8dfe61b0e833164a0c0b63a8a0fc38bf42b1bc
iskur <32363199+iskur@users.noreply.github.com>
2021-09-16 15:48:42 +0200

    BLK

--- weathercop/plotting.py
+++ weathercop/plotting.py
@@ -8,7 +8,7 @@ from weathercop import stats
 
 
 def rel_ranks(data, method="average"):
-    return (spstats.rankdata(data, method) - .5) / len(data)
+    return (spstats.rankdata(data, method) - 0.5) / len(data)
 
 
 def cache(*names, **name_values):
@@ -21,15 +21,18 @@ def cache(*names, **name_values):
     ---------
     *names : str
     """
+
     def wrapper(function):
         @functools.wraps(function)
         def cache_holder(*args, **kwds):
             return function(*args, **kwds)
+
         cache_holder._cache_names = names
         cache_holder._cache_name_values = name_values
         cache_holder.clear_cache = lambda: clear_def_cache(cache_holder)
         cache_holder.clear_cache()
         return cache_holder
+
     return wrapper
 
 
@@ -55,18 +58,34 @@ def clear_def_cache(function, cache_names=None, cache_name_values=None):
         setattr(function, name, value)
 
 
-def ccplom(data, k=0, kind="img", transform=False, varnames=None,
-         h_kwds=None, s_kwds=None, title=None, opacity=.1,
-         cmap=None, x_bins=15, y_bins=15, display_rho=True,
-         display_asy=True, vmax_fct=1., fontsize=None,
-         fontcolor="yellow", scatter=True,
-         axs=None, fig=None, **fig_kwds):
+def ccplom(
+    data,
+    k=0,
+    kind="img",
+    transform=False,
+    varnames=None,
+    h_kwds=None,
+    s_kwds=None,
+    title=None,
+    opacity=0.1,
+    cmap=None,
+    x_bins=15,
+    y_bins=15,
+    display_rho=True,
+    display_asy=True,
+    vmax_fct=1.0,
+    fontsize=None,
+    fontcolor="yellow",
+    scatter=True,
+    axs=None,
+    fig=None,
+    **fig_kwds
+):
     """Cross-Copula-plot matrix. Values that appear on the x-axes are shifted
     back k timesteps. Data is assumed to be a 2 dim arrays with
     observations in rows."""
     if transform:
-        ranks = np.array([stats.rel_ranks(values)
-                         for values in data])
+        ranks = np.array([stats.rel_ranks(values) for values in data])
     else:
         ranks = np.asarray(data)
     K, T = data.shape
@@ -85,11 +104,14 @@ def ccplom(data, k=0, kind="img", transform=False, varnames=None,
         n_variables = 1
 
     if fig is None:
-        fig, axs = plt.subplots(n_variables, n_variables,
-                                subplot_kw=dict(aspect="equal"),
-                                **fig_kwds)
+        fig, axs = plt.subplots(
+            n_variables,
+            n_variables,
+            subplot_kw=dict(aspect="equal"),
+            **fig_kwds
+        )
     if n_variables == 1:
-        axs = (axs,),
+        axs = ((axs,),)
 
     x_slice = slice(None, None if k == 0 else -k)
     y_slice = slice(k, None)
@@ -103,38 +125,64 @@ def ccplom(data, k=0, kind="img", transform=False, varnames=None,
                 continue
             ranks_x = ranks[jj, x_slice]
             ranks_y = ranks[ii, y_slice]
-            hist2d(ranks_x, ranks_y, x_bins, y_bins, ax=ax, cmap=cmap,
-                   scatter=False, kind=kind)
+            hist2d(
+                ranks_x,
+                ranks_y,
+                x_bins,
+                y_bins,
+                ax=ax,
+                cmap=cmap,
+                scatter=False,
+                kind=kind,
+            )
             if scatter:
-                ax.scatter(ranks_x, ranks_y,
-                           marker="o", facecolors=(0, 0, 0, 0),
-                           edgecolors=(0, 0, 0, opacity), **s_kwds)
+                ax.scatter(
+                    ranks_x,
+                    ranks_y,
+                    marker="o",
+                    facecolors=(0, 0, 0, 0),
+                    edgecolors=(0, 0, 0, opacity),
+                    **s_kwds
+                )
             if display_rho:
                 rho = stats.spearmans_rank(ranks_x, ranks_y)
-                ax.text(.5, .5, r"$\rho = %.3f$" % rho,
-                        fontsize=fontsize, color=fontcolor,
-                        horizontalalignment="center")
+                ax.text(
+                    0.5,
+                    0.5,
+                    r"$\rho = %.3f$" % rho,
+                    fontsize=fontsize,
+                    color=fontcolor,
+                    horizontalalignment="center",
+                )
             if display_asy:
                 asy1 = stats.asymmetry1(ranks_x, ranks_y)
                 asy2 = stats.asymmetry2(ranks_x, ranks_y)
-                ax.text(.5, .75, r"$a_1 = %.3f$" % asy1,
-                        fontsize=fontsize, color=fontcolor,
-                        horizontalalignment="center")
-                ax.text(.5, .25, r"$a_2 = %.3f$" % asy2,
-                        fontsize=fontsize, color=fontcolor,
-                        horizontalalignment="center")
+                ax.text(
+                    0.5,
+                    0.75,
+                    r"$a_1 = %.3f$" % asy1,
+                    fontsize=fontsize,
+                    color=fontcolor,
+                    horizontalalignment="center",
+                )
+                ax.text(
+                    0.5,
+                    0.25,
+                    r"$a_2 = %.3f$" % asy2,
+                    fontsize=fontsize,
+                    color=fontcolor,
+                    horizontalalignment="center",
+                )
             ax.set_xlim(0, 1)
             ax.set_ylim(0, 1)
             ax.grid(False)
             ax.set_yticklabels("")
             ax.set_xticklabels("")
             if (jj == 0) or (jj == 1 and ii == 0):
-                ax.set_ylabel(varnames[ii] +
-                              ("(t)" if k else ""))
+                ax.set_ylabel(varnames[ii] + ("(t)" if k else ""))
             K = n_variables
             if (ii == K - 1) or (ii == K - 2 and jj == K - 1):
-                ax.set_xlabel(varnames[jj] +
-                              (("(t-%d)" % k) if k else ""))
+                ax.set_xlabel(varnames[jj] + (("(t-%d)" % k) if k else ""))
     # reset the vlims, so that we have the same color scale in all plots
     for ax in np.ravel(axs):
         for im in ax.get_images():
@@ -148,8 +196,18 @@ def ccplom(data, k=0, kind="img", transform=False, varnames=None,
 
 
 @cache(h_max=-np.inf)
-def hist2d(x, y, n_xbins=15, n_ybins=15, kind="img", ax=None, cmap=None,
-           scatter=True, opacity=.6, vmax=None):
+def hist2d(
+    x,
+    y,
+    n_xbins=15,
+    n_ybins=15,
+    kind="img",
+    ax=None,
+    cmap=None,
+    scatter=True,
+    opacity=0.6,
+    vmax=None,
+):
     if ax is None:
         fig, ax = plt.subplots()
     else:
@@ -165,27 +223,46 @@ def hist2d(x, y, n_xbins=15, n_ybins=15, kind="img", ax=None, cmap=None,
     if h_max > hist2d.h_max:
         hist2d.h_max = h_max
     if kind.startswith("contour"):
-        x_bins = .5 * (xedges[1:] + xedges[:-1])
-        y_bins = .5 * (yedges[1:] + yedges[:-1])
+        x_bins = 0.5 * (xedges[1:] + xedges[:-1])
+        y_bins = 0.5 * (yedges[1:] + yedges[:-1])
         if kind == "contourf":
             ax.contourf(x_bins, y_bins, H.T, cmap=cmap, vmin=0, vmax=vmax)
         elif kind == "contour":
             ax.contour(x_bins, y_bins, H.T, cmap=cmap, vmin=0, vmax=vmax)
     elif kind == "img":
-        ax.imshow(H.T,
-                  extent=(xedges[0], xedges[-1], yedges[0], yedges[-1]),
-                  origin="left", aspect="equal", interpolation="none",
-                  cmap=cmap, vmin=0, vmax=vmax)
-        x = (x - x.min() - .5 / n_xbins) * n_xbins
-        y = (y - y.min() - .5 / n_ybins) * n_ybins
+        ax.imshow(
+            H.T,
+            extent=(xedges[0], xedges[-1], yedges[0], yedges[-1]),
+            origin="left",
+            aspect="equal",
+            interpolation="none",
+            cmap=cmap,
+            vmin=0,
+            vmax=vmax,
+        )
+        x = (x - x.min() - 0.5 / n_xbins) * n_xbins
+        y = (y - y.min() - 0.5 / n_ybins) * n_ybins
     if scatter:
-        ax.scatter(x, y, marker="o", facecolors=(0, 0, 0, 0),
-                   edgecolors=(0, 0, 0, opacity))
+        ax.scatter(
+            x,
+            y,
+            marker="o",
+            facecolors=(0, 0, 0, 0),
+            edgecolors=(0, 0, 0, opacity),
+        )
     return fig, ax
 
 
-def plot_cross_corr(data, varnames=None, max_lags=10, figsize=None, fig=None,
-                    axs=None, *args, **kwds):
+def plot_cross_corr(
+    data,
+    varnames=None,
+    max_lags=10,
+    figsize=None,
+    fig=None,
+    axs=None,
+    *args,
+    **kwds
+):
     K = data.shape[0]
     if varnames is None:
         varnames = np.arange(K).astype(str)
@@ -204,11 +281,12 @@ def plot_cross_corr(data, varnames=None, max_lags=10, figsize=None, fig=None,
             # fig and axs
             colors = plt.rcParams["axes.prop_cycle"]
             color = colors[var_j % len(colors)]
-            lines += axs[var_i].plot(lags, cross_corrs[:, var_i, var_j],
-                                     color=color, *args, **kwds)
+            lines += axs[var_i].plot(
+                lags, cross_corrs[:, var_i, var_j], color=color, *args, **kwds
+            )
         axs[var_i].set_title(varnames[var_i])
         axs[var_i].grid(True)
-    plt.subplots_adjust(right=.75, hspace=.25)
+    plt.subplots_adjust(right=0.75, hspace=0.25)
     fig.legend(lines, varnames, loc="center right")
     return fig, axs
 
@@ -225,8 +303,7 @@ def cross_corr(data, k):
 
 def nanavg(x, axis=None, ddof=0):
     ii = np.isfinite(x)
-    return (np.nansum(x, axis=axis) /
-            (np.sum(ii, axis=axis) - ddof))
+    return np.nansum(x, axis=axis) / (np.sum(ii, axis=axis) - ddof)
 
 
 def cross_cov(data, k, means=None):
@@ -242,7 +319,8 @@ def cross_cov(data, k, means=None):
     cross = np.empty((n_vars, n_vars))
     for ii in range(n_vars):
         for jj in range(n_vars):
-            cross[ii, jj] = nanavg((data[ii, :k_right] - means[ii]) *
-                                   (data[jj, k:] - means[jj]),
-                                   ddof=ddof)
+            cross[ii, jj] = nanavg(
+                (data[ii, :k_right] - means[ii]) * (data[jj, k:] - means[jj]),
+                ddof=ddof,
+            )
     return cross
