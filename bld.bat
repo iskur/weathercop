2e510f5615afc4a815320b3b3ae3883273979c35
iskur <32363199+iskur@users.noreply.github.com>
2023-03-28 14:22:30 +0200

    MAINT: Compilation and packaging woes

--- bld.bat
+++ bld.bat
@@ -1,2 +1,6 @@
-"%PYTHON%" setup.py install
+set CC=icl
+set LD=xilink
+
+python setup.py build --force --compiler=intelemw install --old-and-unmanageable
+rem python setup.py install
 if errorlevel 1 exit 1
--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-$PYTHON $RECIPE_DIR setup.py install     # Python command to install the script.
+CFLAGS="-I$PREFIX/include $CFLAGS" $PYTHON $RECIPE_DIR setup.py build --force --compiler=intelem install --old-and-unmanageable
+# $PYTHON $RECIPE_DIR setup.py install     # Python command to install the script.
